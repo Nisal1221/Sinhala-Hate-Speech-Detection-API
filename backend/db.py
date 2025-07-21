feaf702a86e30307d7bbbd6a6188ae0b29ce28d1
Nisal_Basnayake <140801128+Nisal1221@users.noreply.github.com>
2025-07-21 17:43:51 +0530

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,1 +1,17 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+load_dotenv()
+
+# Get URI from environment
+MONGODB_URI = os.getenv("MONGODB_URI")
+
+# Connect to MongoDB
+client = MongoClient(MONGODB_URI)
+
+# Use the database and collection
+db = client["hate_speech_db"]
+detections_collection = db["detections"]
 
