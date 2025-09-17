80a5fce4daaa128acc5718b290c926237a1ae9bd
Nisal_Basnayake <140801128+Nisal1221@users.noreply.github.com>
2025-09-17 21:41:19 +0530

    Update test.py
    hidden for public safe

--- backend/token_level/test.py
+++ backend/token_level/test.py
@@ -8,7 +8,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_path)
 model = AutoModelForTokenClassification.from_pretrained(model_path)
 labels = joblib.load(f"{model_path}/label_encoder.pkl")  # ["O", "Hate"]
 
-# ✅Set device
+# Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 model.eval()
